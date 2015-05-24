0d2d269a491f863513fb504df927992f484630d4
grnflash <grnflash@gmail.com>
2015-05-23 21:09:57 -0500

    Programming Assignment 2
    Create a function that can create and cache matrices.  Create a second
    function to create and pass-back the inverse of those matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions create and manipulate 
+##   a cached matrix and its inverse.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix creates a matrix by passing a 
+##   variety of data and optional parameters.
+makeCacheMatrix <- function(x=matrix()) {
+        i <- NULL  ## initializes i
+        set <- function(dat, ...) {  ## input data/params 
+                x <<- matrix(data=dat, ...)  ## creates matrix
+                i <<- NULL  ## stores inv &/or signals new values
+        }
+        get <- function() x  ## retrieves user-created matrix
+        setinv <- function(ivx) i <<- ivx  ## insert calc'd inv
+        getinv <- function() i  ## retrieve calculated inv
+        list(set=set, get=get, setinv=setinv, ## store func
+             getinv=getinv)  ##   "
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve creates & caches an inverse matrix from 
+##   the user-defined matrix. It also retrieves a cached 
+##   inverse matrix where one already exists.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        im <- x$getinv()  ## retrieve value from cached inv matrix
+        if(!is.null(im)) {  ## eval's for existing inv matrix
+                message("getting cached data")  ## inv already cached
+                return(invisible(im))  ## return existing inv matrix
+                        ## made invisible since redundant with getinv()
+        }
+        data <- x$get() ## (if no existing inv) retrieve orig matrix
+        im <- solve(data)  ## create inv matrix from user-def'd matrix
+        x$setinv(im)  ## pass inv matrix back to makeCacheMatrix
+        invisible(im)  ## returns inv matrix as good practice
+                ## again, invisible, since redundant w/ getinv()
 }
